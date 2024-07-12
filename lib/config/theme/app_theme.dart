3f1ba088d3dfead4a3c7285bc0fc9c1ab5994d6e
AJMG-95 <antoniojesus.marchena@iesdonana.org>
2024-07-12 19:22:25 +0200

    feat(app_theme.dart): Estilos de appbar

---
+++ lib/config/theme/app_theme.dart
@@ -0,0 +1,30 @@
+import 'package:flutter/material.dart';
+
+const colorList = <Color>[
+  Color(0xFF001F3F),
+  Color(0xFF0074D9),
+  Color(0xFF2ECC40),
+  Color(0xFF3D9970),
+  Color(0xFFFFDC00),
+  Color(0xFFFF851B),
+  Color(0xFFFF4136),
+  Color(0xFFAAAAAA),
+  Color(0xFFFFFFFF),
+];
+
+class AppTheme {
+  final int selectedColor;
+
+  AppTheme({
+    this.selectedColor = 0
+    }): assert(selectedColor >= 0, 'Selected color must be grater than 0'),
+        assert(selectedColor < colorList.length , 'Selected color must be less or equal than ${colorList.length - 1}');
+
+  ThemeData getTheme() => ThemeData(
+    useMaterial3: true,
+    colorSchemeSeed: colorList[selectedColor],
+    appBarTheme: const AppBarTheme(
+      centerTitle: false,
+    )
+  );
+}
