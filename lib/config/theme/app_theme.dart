ae9d74688d03f1860e5c0ac0cc92d9f35d8def1c
AJMG-95 <antoniojesus.marchena@iesdonana.org>
2024-08-10 11:20:42 +0200

    feat(home_screen.dart): implements home screen

--- lib/config/theme/app_theme.dart
+++ lib/config/theme/app_theme.dart
@@ -24,7 +24,7 @@ class AppTheme {
     useMaterial3: true,
     colorSchemeSeed: colorList[selectedColor],
     appBarTheme: const AppBarTheme(
-      centerTitle: false,
+      centerTitle: true,
     )
   );
 }
