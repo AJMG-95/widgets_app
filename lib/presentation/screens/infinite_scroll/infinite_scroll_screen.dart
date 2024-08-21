3722d191b5756bb56464de8847b286b201d740fe
AJMG-95 <antoniojesus.marchena@iesdonana.org>
2024-08-21 19:49:04 +0200

    feat(infinite_scroll_screen): añade un ScrollController con su ciclo e vida y emula una carga asincrona

--- lib/presentation/screens/infinite_scroll/infinite_scroll_screen.dart
+++ lib/presentation/screens/infinite_scroll/infinite_scroll_screen.dart
@@ -11,6 +11,66 @@ class InfiniteScrollScreen extends StatefulWidget {
 
 class _InfiniteScrollScreenState extends State<InfiniteScrollScreen> {
   List<int> imagesIds = [0, 1, 2, 3, 4, 5];
+  final ScrollController scrollController = ScrollController();
+  //* Bandera para simular carga en conexion asincrona
+  bool isLoading = false;
+  //* Bandera para comprobar que el usuario no se sale durante la carga de nuevos elementos
+  bool isMounted = true;
+
+  //* Al trbajajar con un controlador hay que iniciar/terminar el ciclo de vida del mismo
+  //* En el initState el super.initState debe estar al pricipio
+  @override
+  void initState() {
+    super.initState();
+    scrollController.addListener(() {
+      //Posicion actual: scrollController.position.pixels;
+      //Posicion maxima alcanzable: scrollController.position.maxScrollExtent;
+
+      if ((scrollController.position.pixels + 500) >=
+          scrollController.position.maxScrollExtent) {
+        //* El método addFiveImages es inmediato así que se implementa
+        // * este método para simular una conexión asincrona, en la que se espera a
+        // * la respuesta del servidor
+        loadNextPage();
+      }
+    });
+  }
+
+  //* En el dispose el super.disposer debe estar al final
+  //* El dispose se llama cuando el componente está siendo destruido o está marcado para ser destruido
+  @override
+  void dispose() {
+    scrollController.dispose();
+    //* Se modifica el valor aqui, en el punto en el que el componente va a ser destruido
+    isMounted = true;
+    super.dispose();
+  }
+
+  Future loadNextPage() async {
+    //* Previene que se lance la petición Si ya está cargando
+    if (isLoading) return;
+    isLoading = true;
+    setState(() {});
+    //? Hasta este punto todo era sincrono
+    await Future.delayed(const Duration(seconds: 2));
+    addFiveImages();
+    isLoading = false;
+    //* Revisar si está montado el componente / widget
+    // Como la carga es asincrona, si el usuario se sale pop durante la carga,
+    // Se puede lanzar el setState en el componente / y producir exepcions y/o romper la app
+    //? Aquí es donde hay que revisar si todo está montado o no
+    if (!isMounted) return;
+
+    setState(() {});
+  }
+
+  void addFiveImages() {
+    //* .last obtiene el ultimo valor de una lista
+    final lastId = imagesIds.last;
+
+    //* .addAll añade todos los resultados del map, a la lista sobre la que se ejecuta
+    imagesIds.addAll([1, 2, 3, 4, 5].map((e) => lastId + e));
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -27,26 +87,26 @@ class _InfiniteScrollScreenState extends State<InfiniteScrollScreen> {
         context: context,
         removeTop: true,
         removeBottom: true,
-          //* El builer hace que los elementos se construyan bajo demanda, y no tener el
-          // * Screen con todos los elementos cargados, ya que sería poco eficiente
-          // * El builer sabe cuando estan apunto de entrar en pantalla para así generarlos
-          // * Tambien maneja en caché las imagenes por lo que no se van a perder (es muy eficiente)
+        //* El builer hace que los elementos se construyan bajo demanda, y no tener el
+        // * Screen con todos los elementos cargados, ya que sería poco eficiente
+        // * El builer sabe cuando estan apunto de entrar en pantalla para así generarlos
+        // * Tambien maneja en caché las imagenes por lo que no se van a perder (es muy eficiente)
         child: ListView.builder(
-          //* indica la cantidad de elementos que se deben/pueden cargar
-          itemCount: imagesIds.length,
-          //* Construye los elementos a cargar
-          itemBuilder: (context, index) {
-          // * Permite cargar una imagen y mientras se carga la imagen muestra un placeholder
-          return FadeInImage(
-            fit: BoxFit.cover,
-            width: double.infinity,
-            height: 300,
-            placeholder: const AssetImage('assets/images/jar-loading.gif'),
-            image: NetworkImage(
-                'https://picsum.photos/id/${imagesIds[index]}/500/300'),
-          );
-          }
-        ),
+            controller: scrollController,
+            //* indica la cantidad de elementos que se deben/pueden cargar
+            itemCount: imagesIds.length,
+            //* Construye los elementos a cargar
+            itemBuilder: (context, index) {
+              // * Permite cargar una imagen y mientras se carga la imagen muestra un placeholder
+              return FadeInImage(
+                fit: BoxFit.cover,
+                width: double.infinity,
+                height: 300,
+                placeholder: const AssetImage('assets/images/jar-loading.gif'),
+                image: NetworkImage(
+                    'https://picsum.photos/id/${imagesIds[index]}/500/300'),
+              );
+            }),
       ),
 
       floatingActionButton: FloatingActionButton(
