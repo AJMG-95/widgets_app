e34e7d71499cb10f965f9dfa53e75247a4ae0952
AJMG-95 <antoniojesus.marchena@iesdonana.org>
2024-09-07 11:54:14 +0200

    feat(theme_rpvider)

--- lib/presentation/providers/counter_provider.dart
+++ lib/presentation/providers/counter_provider.dart
@@ -2,4 +2,4 @@ import 'package:flutter_riverpod/flutter_riverpod.dart';
 
 //* StateProvider es un proveedor de estado (Tema seleccionado, valor de
 //* variables, estado de alguna clase)
-final counterProvider = StateProvider((ref) => 5);
+final counterProvider = StateProvider<int>((ref) => 5);
