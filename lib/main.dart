42dff068372a8332ddba0f35d0049f655c4acfc7
AJMG-95 <antoniojesus.marchena@iesdonana.org>
2024-09-07 11:12:37 +0200

    feat(counter_screen): cambia a ConsumerWidget y usa WidgetRef->counterProvider

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,17 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:widgets_app/config/router/app_router.dart';
 import 'package:widgets_app/config/theme/app_theme.dart';
 
-
 void main() {
-  runApp(const MainApp());
+  runApp(
+    //* ProviderScope es un tipo de widget especial que va a mantener una
+    // * Refeerencia a todos los providers que se utilicen
+    // * Gracias a ProviderScope riverpod va a saber donde buscar los providers
+    // * que se vayan creando (providers de riverpod)
+    const ProviderScope(child: MainApp() )
+  );
+
 }
 
 class MainApp extends StatelessWidget {
--- lib/presentation/screens/counter/counter_screen.dart
+++ lib/presentation/screens/counter/counter_screen.dart
@@ -1,30 +1,36 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:widgets_app/presentation/providers/counter_provider.dart';
 
-class CounterScreen extends StatefulWidget {
+//* Extender ConsumerWidget en lugar de StatelessWidget, que es expuesto por Riverpod
+class CounterScreen extends ConsumerWidget {
   static const String name = "counter_screen";
   const CounterScreen({super.key});
 
+  //* el builder de un ConsumerWidget, ofrece la referencia a un WidgetRef,
+  // * que es como decirle a riverpod que se va a ocupar la referencia a
+  // * algún provider de riverpod
   @override
-  State<CounterScreen> createState() => _CounterScreenState();
-}
+  Widget build(BuildContext context, WidgetRef ref) {
+    final colors = Theme.of(context).colorScheme;
 
-class _CounterScreenState extends State<CounterScreen> {
-  @override
-  Widget build(BuildContext context) {
+    //* Aqui es donde se espercifica el provider que se va a usar
+    // ref.watch hace que se esté a la "escucha" del provider, y cada vez
+    //que este cambie, automaticamente flutter va a tomar el nuevo valor/stado
+    final int clickCounter = ref.watch( counterProvider );
+
     return Scaffold(
       appBar: AppBar(
         title: const Text("counter_screen"),
       ),
       body: Center(
-        child: Text(
-          "Valor",
-          style: Theme.of(context).textTheme.titleLarge
-        )
-      ),
+          child:
+              Text("Valor: $clickCounter", style: Theme.of(context).textTheme.titleLarge)),
       floatingActionButton:
           FloatingActionButton(
-            child: const Icon(Icons.add),
-            onPressed: () {}
+            onPressed: () {},
+            backgroundColor: colors.primary ,
+            child:  const Icon(Icons.add),
           ),
     );
   }
